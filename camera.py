78ed5cc44f605e3921c42a92849ef5629dd8cc28
Lf Laguardia <laguardialuis@gmail.com>
2021-06-05 20:38:31 -0300

    expliquei as paradas q eu escrevi

--- camera.py
+++ camera.py
@@ -29,15 +29,28 @@ class camera:
         self._pos = pos
 
 
+
+class objeto:
+    def __init__(self, pos=[0,0], kind = None, sprite="circle", speed = [0, 0]):
+        self.pos = pos
+        self.type = kind
+        self.sprite = sprite
+        self.speed = speed
+    
+    def move(self):
+        self.pos[0] += self.speed[0]
+        self.pos[1] += self.speed[1]
+        
+        
+        
          
 cam = camera([0, 0], (400, 400))
 
-
 pygame.init()
-
 win = pygame.display.set_mode(cam.props)
-
 pygame.display.set_caption("Game")
+
+objs_list = []
 
 run = True
 while run:
@@ -49,24 +62,44 @@ while run:
             run = False
 
     if keys[pygame.K_LEFT]:
-        char['pos'][0] -= 0.03
+        char['pos'][0] -= 0.1
     
     if keys[pygame.K_RIGHT]:
-        char['pos'][0] += 0.03
+        char['pos'][0] += 0.1
         
     if keys[pygame.K_UP]:
-        char['pos'][1] -= 0.03
+        char['pos'][1] -= 0.1
     
     if keys[pygame.K_DOWN]:
-        char['pos'][1] += 0.03
+        char['pos'][1] += 0.1
+    
+    if keys[pygame.K_a]:
+        bullet = objeto(pos=list(char['pos']), kind = "bullet", speed=[0.1, 0])
+        objs_list.append(bullet)
 	
+    #posiciona a camera bonito de acordo com o player e mouse
     cam.pos = [char['pos'][0]+5+(pygame.mouse.get_pos()[0]-cam.props[0]/2)/5, \
                       char['pos'][1]+5+(pygame.mouse.get_pos()[1]-cam.props[1]/2)/5]
     
+    #desenha o personagem, o fundo e um circulo
     win.fill((156, 132, 109))
     pygame.draw.circle(win, (120, 91, 76), cam.render((50, 50)), 20)
     pygame.draw.circle(win, char["color"], cam.render(char["pos"]), 10)
+    
+    #exemplo de desenho em cima da camera
     pygame.draw.rect(win, (50, 252, 108), (5, 5, 150, 10))
+    
+    #pra cada objeto na lista de objetos
+    for obj in objs_list:
+        obj.move() #move o objeto
+        
+        #desenha o objeto
+        if obj.sprite == "circle":
+            pygame.draw.circle(win, (0, 0, 0), cam.render(obj.pos), 5)
+        
+        #se o objeto tá mt longe, destroi
+        if ((obj.pos[0]-char['pos'][0])**2 + (obj.pos[1]-char['pos'][1])**2)**(1/2) > 400:
+            objs_list.remove(obj)
 
     pygame.display.update()
 
