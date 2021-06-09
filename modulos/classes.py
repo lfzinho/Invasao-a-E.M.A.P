853019656e0f88695ce83f97d62313189476d4e2
Gustavo Sanches <gsanchescosta@yahoo.com.br>
2021-06-09 11:48:07 -0300

    alteração do sprite idle

--- modulos/classes.py
+++ modulos/classes.py
@@ -6,7 +6,7 @@ class Soldado(pygame.sprite.Sprite):
 
     def __init__(self, x, y, escala):
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load(os.path.join("sprites", "idle", "0.png"))
+        img = pygame.image.load(os.path.join("sprites", "idle.png"))
         self.image = pygame.transform.scale(img, (int(img.get_width() * escala), int(img.get_height() * escala)))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
