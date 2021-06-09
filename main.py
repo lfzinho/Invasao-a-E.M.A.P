b7e5040ccd7b71a3eabd2be337b4bd51b3226bf7
Gustavo Sanches <gsanchescosta@yahoo.com.br>
2021-06-09 12:45:40 -0300

    adicionado a movimentação do personagem

--- main.py
+++ main.py
@@ -8,20 +8,65 @@ largura_tela = 800
 altura_tela = int(largura_tela * 0.8)
 tela = pygame.display.set_mode((largura_tela, altura_tela))
 
+# Altera o título e o ícone da aba
 pygame.display.set_caption("Sonia's Fury")
 icon = pygame.image.load(os.path.join("sprites", "flame_icon.png"))
 pygame.display.set_icon(icon)
 
-jogador = classes.Soldado(200, 200, 2)
+# Define FPS
+clock = pygame.time.Clock()
+FPS = 60
+
+# Cria objetos da classe Soldado
+jogador = classes.Soldado("jogador", 200, 200, 2, 5)
+inimigo = classes.Soldado("inimigo", 400, 200, 2, 5)
+
+movendo_direita = False
+movendo_esquerda = False
+
+# Define a cores
+fundo = (144, 201, 220)
+
+
+def desenha_fundo():
+    """
+    desenha o fundo toda vez que é chamada
+    """
+    tela.fill(fundo)
+
 
 rodando = True
 while rodando:
 
-    tela.blit(jogador.image, jogador.rect)
+    clock.tick(FPS)
+
+    desenha_fundo()
+
+    jogador.draw(tela)
+    inimigo.draw(tela)
+
+    jogador.move(movendo_esquerda, movendo_direita)
 
     for event in pygame.event.get():
+        # Sai do jogo quando a aba for fechada
         if event.type == pygame.QUIT:
             rodando = False
+
+        # Verifica teclas apertadas
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                movendo_esquerda = True
+            if event.key == pygame.K_d:
+                movendo_direita = True
+
+        # Verifica teclas soltas
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                movendo_esquerda = False
+            if event.key == pygame.K_d:
+                movendo_direita = False
+            if event.key == pygame.K_ESCAPE:
+                rodando = False
 
     pygame.display.update()
 
--- modulos/classes.py
+++ modulos/classes.py
@@ -3,10 +3,48 @@ import os
 
 
 class Soldado(pygame.sprite.Sprite):
-
-    def __init__(self, x, y, escala):
+    """
+    Classe para jogadores e inimigos
+    """
+    def __init__(self, tipo, x, y, escala, velocidade):
         pygame.sprite.Sprite.__init__(self)
-        img = pygame.image.load(os.path.join("sprites", "idle.png"))
+        self.velocidade = velocidade
+        self.direction = 1
+        self.flip = False
+        self.tipo = tipo
+
+        img = pygame.image.load(os.path.join("sprites", self.tipo, "idle.png"))
         self.image = pygame.transform.scale(img, (int(img.get_width() * escala), int(img.get_height() * escala)))
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+
+    def move(self, movendo_esquerda, movendo_direita):
+        """
+        verifica se o personagem deveria estar se movendo, e altera sua posição em x e y.
+        Também verifica se o sprite necessita ser espelhado.
+        :param movendo_esquerda: False ou True
+        :param movendo_direita: False ou True
+        :return:
+        """
+        dx = 0
+        dy = 0
+
+        if movendo_esquerda:
+            dx = -self.velocidade
+            self.direction = -1
+            self.flip = True
+        if movendo_direita:
+            dx = self.velocidade
+            self.direction = 1
+            self.flip = False
+
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def draw(self, tela):
+        """
+        Desenha o personagem na tela
+        :param tela: objeto tela
+        :return:
+        """
+        tela.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
