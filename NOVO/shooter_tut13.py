db4d4edb1b0491486bb1b4bafa8cf918786185dd
Gustavo Sanches <gsanchescosta@yahoo.com.br>
2021-06-14 11:22:38 -0300

    Diminui levemente o tamanho do player

--- NOVO/shooter_tut13.py
+++ NOVO/shooter_tut13.py
@@ -435,7 +435,7 @@ class World():
                         decoration = Decoration(img, x * TILE_SIZE, y * TILE_SIZE)
                         decoration_group.add(decoration)
                     elif tile == 15: # create player
-                        player = Soldier('player', x * TILE_SIZE, y * TILE_SIZE, 1.65, 5, 20, 5)
+                        player = Soldier('player', x * TILE_SIZE, y * TILE_SIZE, 1.4, 5, 20, 5)
                         health_bar = HealthBar(10, 10, player.health, player.health)
                     elif tile == 16: # create enemies
                         enemy = Soldier('enemy', x * TILE_SIZE, y * TILE_SIZE, 1.65, 2, 20, 0)
