07a466d8696964381d250f80406c0cdbde52f066
Gustavo Sanches <gsanchescosta@yahoo.com.br>
2021-06-14 10:40:58 -0300

    melhorias na inversão de gravidade

--- NOVO/shooter_tut13.py
+++ NOVO/shooter_tut13.py
@@ -15,11 +15,11 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Shooter')
 
-#set framerate
+# set framerate
 clock = pygame.time.Clock()
 FPS = 60
 
-#define game variables
+# define game variables
 GRAVITY = 0.75
 SCROLL_THRESH = 280
 DEFAULT_ROWS = 16
@@ -36,7 +36,7 @@ start_game = False
 start_intro = False
 
 
-#define player action variables
+# define player action variables
 moving_left = False
 moving_right = False
 shoot = False
@@ -44,10 +44,10 @@ grenade = False
 grenade_thrown = False
 
 
-#load music and sounds
-#pygame.mixer.music.load('audio/music2.mp3')
-#pygame.mixer.music.set_volume(0.3)
-#pygame.mixer.music.play(-1, 0.0, 5000)
+# load music and sounds
+# pygame.mixer.music.load('audio/music2.mp3')
+# pygame.mixer.music.set_volume(0.3)
+# pygame.mixer.music.play(-1, 0.0, 5000)
 jump_fx = pygame.mixer.Sound('audio/jump.wav')
 jump_fx.set_volume(0.05)
 shot_fx = pygame.mixer.Sound('audio/shot.wav')
@@ -56,27 +56,27 @@ grenade_fx = pygame.mixer.Sound('audio/grenade.wav')
 grenade_fx.set_volume(0.05)
 
 
-#load images
-#button images
+# load images
+# button images
 start_img = pygame.image.load('img/start_btn.png').convert_alpha()
 exit_img = pygame.image.load('img/exit_btn.png').convert_alpha()
 restart_img = pygame.image.load('img/restart_btn.png').convert_alpha()
-#background
+# background
 pine1_img = pygame.image.load('img/Background/pine1.png').convert_alpha()
 pine2_img = pygame.image.load('img/Background/pine2.png').convert_alpha()
 mountain_img = pygame.image.load('img/Background/mountain.png').convert_alpha()
 sky_img = pygame.image.load('img/Background/sky_cloud.png').convert_alpha()
-#store tiles in a list
+# store tiles in a list
 img_list = []
 for x in range(TILE_TYPES):
     img = pygame.image.load(f'img/Tile/{x}.png')
     img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
     img_list.append(img)
-#bullet
+# bullet
 bullet_img = pygame.image.load('img/icons/bullet.png').convert_alpha()
-#grenade
+# grenade
 grenade_img = pygame.image.load('img/icons/grenade.png').convert_alpha()
-#pick up boxes
+# pick up boxes
 health_box_img = pygame.image.load('img/icons/health_box.png').convert_alpha()
 ammo_box_img = pygame.image.load('img/icons/ammo_box.png').convert_alpha()
 grenade_box_img = pygame.image.load('img/icons/grenade_box.png').convert_alpha()
@@ -87,7 +87,7 @@ item_boxes = {
 }
 
 
-#define colours
+# define colours
 BG = (144, 201, 120)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
@@ -112,6 +112,17 @@ def draw_bg():
         screen.blit(pine1_img, ((x * width) - bg_scroll * 0.7, SCREEN_HEIGHT - pine1_img.get_height() - 150))
         screen.blit(pine2_img, ((x * width) - bg_scroll * 0.8, SCREEN_HEIGHT - pine2_img.get_height()))
 
+
+
+def draw_mist():
+    s = pygame.Surface((1000, 750))  # the size of your rect
+    if player.gravity == 1:
+        s.set_alpha(0)  # alpha level
+    if player.gravity == -1:
+        s.set_alpha(90)  # alpha level
+
+    s.fill((50, 0, 100))
+    screen.blit(s, (0, 0))
 
 #function to reset level
 def reset_level():
@@ -156,6 +167,7 @@ class Soldier(pygame.sprite.Sprite):
         self.flip = False
         self.flipy = False
         self.gravity = 1
+        self.flag = 1
         self.animation_list = []
         self.frame_index = 0
         self.action = 0
@@ -286,8 +298,16 @@ class Soldier(pygame.sprite.Sprite):
 
         if pygame.sprite.spritecollide(self, portal_group, False):
             if self.char_type != "enemy":
-                self.gravity = -1
-                self.flipy = True
+                if self.gravity == 1 and self.flag == 1:
+                    self.gravity = -1
+                    self.flipy = True
+                    self.flag = 0
+                elif self.gravity == -1 and self.flag == 1:
+                    self.gravity = 1
+                    self.flipy = False
+                    self.flag = 0
+        elif not(pygame.sprite.spritecollide(self, portal_group, False)):
+            self.flag = 1
 
         #check for collision with exit
         level_complete = False
@@ -770,6 +790,8 @@ while run:
         water_group.draw(screen)
         portal_group.draw(screen)
         exit_group.draw(screen)
+
+        draw_mist()
 
         #show intro
         if start_intro == True:
